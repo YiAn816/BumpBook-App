c1b92784a2b11988930e507e6b7d4749e693129e
ccQi <qi.cheng116@qq.com>
2024-03-20 10:40:30 +0800

    初始化

---
+++ config.js
@@ -0,0 +1,23 @@
+// 应用全局配置
+module.exports = {
+  baseUrl: 'http://127.0.0.1:8080',
+  // 应用信息
+  appInfo: {
+    // 应用名称
+    name: "美孕档案",
+    // 应用版本
+    version: "1.1.0",
+    // 应用logo
+    logo: "/static/logo.png",
+    // 官方网站
+    site_url: "https://ccqi.com",
+    // 政策协议
+    agreements: [{
+        title: "隐私政策",
+      },
+      {
+        title: "用户服务协议",
+      }
+    ]
+  }
+}
